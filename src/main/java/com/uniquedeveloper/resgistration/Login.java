0faf30c148c78ebe488efcdef3c9dae6f71ee182
aabis1 <aabiskarkc16@gmail.com>
2023-12-09 17:48:12 +0900

    initial commit

---
+++ src/main/java/com/uniquedeveloper/resgistration/Login.java
@@ -0,0 +1,67 @@
+package com.uniquedeveloper.resgistration;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+    
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uemail = request.getParameter("username");
+		String upwd = request.getParameter("username");
+		HttpSession session = request.getSession();
+		RequestDispatcher dispatcher = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookstore?useSSL=false","root","12345");
+			PreparedStatement pst = con.prepareStatement("select * from users where uemail = ? and upwd = ?");
+			pst.setString(1, uemail);
+			pst.setString(2, upwd);
+			
+			ResultSet rs = pst.executeQuery();
+			if (rs.next()) {
+				request.setAttribute("", rs.getString("uname"));
+				dispatcher = request.getRequestDispatcher("index.jsp");
+			} else {
+				request.setAttribute("status","failed");
+				dispatcher = request.getRequestDispatcher("login.jsp");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
